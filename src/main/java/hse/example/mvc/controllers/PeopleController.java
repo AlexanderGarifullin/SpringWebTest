afd00320125871f4766ff61ae67490fea13c16bf
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-11 01:39:06 +0500

    feat: add Get new person and Post create person requests

--- src/main/java/hse/example/mvc/controllers/PeopleController.java
+++ src/main/java/hse/example/mvc/controllers/PeopleController.java
@@ -1,12 +1,11 @@
 package hse.example.mvc.controllers;
 
 import hse.example.mvc.dao.PersonDAO;
+import hse.example.mvc.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -31,5 +30,17 @@ public class PeopleController {
         // Получим одного человека по его id из DAO и передадим на отображение в представление
         model.addAttribute("person", personDAO.show(id));
         return "/people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+        return "/people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
