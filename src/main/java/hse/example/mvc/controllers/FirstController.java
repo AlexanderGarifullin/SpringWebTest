d1b7e6a625e61cc1735c287aa24fc69a83073998
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-10 22:08:57 +0500

    feat: add RequestMapping on FirstController

--- src/main/java/hse/example/mvc/controllers/FirstController.java
+++ src/main/java/hse/example/mvc/controllers/FirstController.java
@@ -2,8 +2,10 @@ package hse.example.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(){
