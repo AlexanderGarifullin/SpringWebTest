4dda87316d3c4ed846c19265e65b7482b30f38f4
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-11 03:33:33 +0500

    feat: add fields validation rules in Person model

--- src/main/java/hse/example/mvc/models/Person.java
+++ src/main/java/hse/example/mvc/models/Person.java
@@ -3,12 +3,25 @@ package hse.example.mvc.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 public class Person {
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
+
+    @Min(value = 0, message = "Age should be greater than zero")
     private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
     private String email;
 
     public Person(){}
