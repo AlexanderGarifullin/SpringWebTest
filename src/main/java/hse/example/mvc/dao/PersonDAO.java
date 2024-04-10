65437874156e67014a0a6f208b7581bb6e26d73b
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-11 02:23:16 +0500

    feat: add update method in PersonDAO

--- src/main/java/hse/example/mvc/dao/PersonDAO.java
+++ src/main/java/hse/example/mvc/dao/PersonDAO.java
@@ -31,4 +31,10 @@ public class PersonDAO {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
     }
+
+    public void update(int id, Person updatedPerson){
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
 }
