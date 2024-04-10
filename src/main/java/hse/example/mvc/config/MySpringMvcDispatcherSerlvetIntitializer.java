875ef5d2efdee41272faedf1ed77c7d09ca4d35c
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-11 02:37:01 +0500

    feat: add registerHiddenFieldFilter in MySpringMvcDispatcherSerlvetIntitializer

--- src/main/java/hse/example/mvc/config/MySpringMvcDispatcherSerlvetIntitializer.java
+++ src/main/java/hse/example/mvc/config/MySpringMvcDispatcherSerlvetIntitializer.java
@@ -1,5 +1,8 @@
 package hse.example.mvc.config;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -17,5 +20,16 @@ public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotation
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 }
