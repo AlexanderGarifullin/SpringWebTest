50dfa2a93434c4afaf4d6fe42db81e2718a28bca
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-11 00:20:00 +0500

    feat: add parameters in GetRequest hello

--- src/main/java/hse/example/mvc/controllers/FirstController.java
+++ src/main/java/hse/example/mvc/controllers/FirstController.java
@@ -3,12 +3,15 @@ package hse.example.mvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname){
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
 
