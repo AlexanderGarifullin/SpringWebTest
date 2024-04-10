3abe1fc3dda6b3c11a720a801adeb82d866f5958
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-11 01:07:25 +0500

    feat: add Person model

---
+++ src/main/java/hse/example/mvc/models/Person.java
@@ -0,0 +1,11 @@
+package hse.example.mvc.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Person {
+    private int id;
+    private String name;
+}
