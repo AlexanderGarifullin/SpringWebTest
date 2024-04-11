6b5b016f27acba24f0c0ace4c8a1a4a2cda16808
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-11 06:07:18 +0500

    feat: add redirect controller to PeopleController

---
+++ src/main/java/hse/example/mvc/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package hse.example.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HomeController {
+
+    @RequestMapping("/")
+    public String redirectToPeople() {
+        return "redirect:/people";
+    }
+}
