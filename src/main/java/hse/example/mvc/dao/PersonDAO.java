fb78dbdd214cc0b4cab7ab7a0c9ae57490e24cf8
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-12 17:33:54 +0500

    ref: save in PersonDAO now use serial column in DB table

--- src/main/java/hse/example/mvc/dao/PersonDAO.java
+++ src/main/java/hse/example/mvc/dao/PersonDAO.java
@@ -30,7 +30,7 @@ public class PersonDAO {
     }
 
     public void save(Person person){
-        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)",
+        jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES(?, ?, ?)",
                 person.getName(), person.getAge(), person.getEmail());
     }
 
