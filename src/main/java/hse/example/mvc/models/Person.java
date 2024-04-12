a8a3a63ee8b1507f37e322f1886afac80931cebb
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-12 19:50:54 +0500

    ref: set min value 1 in Person age

--- src/main/java/hse/example/mvc/models/Person.java
+++ src/main/java/hse/example/mvc/models/Person.java
@@ -18,7 +18,7 @@ public class Person {
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
 
-    @Min(value = 0, message = "Age should be greater than zero")
+    @Min(value = 1, message = "Age should be greater than zero")
     private int age;
 
     @NotEmpty(message = "Email should not be empty")
