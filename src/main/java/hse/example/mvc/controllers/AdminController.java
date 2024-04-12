8fee5c49437bcd21916373d7a50c15e7218d2c2b
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-12 20:49:56 +0500

    feat: add AdminController

---
+++ src/main/java/hse/example/mvc/controllers/AdminController.java
@@ -0,0 +1,31 @@
+package hse.example.mvc.controllers;
+
+import hse.example.mvc.dao.PersonDAO;
+import hse.example.mvc.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public AdminController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String admin(Model model, @ModelAttribute("person")Person person) {
+        model.addAttribute("people", personDAO.index());
+        return "adminPage";
+    }
+
+    @PatchMapping("/add")
+    public String makeAdmin(@ModelAttribute("person") Person person) {
+        System.out.println(person.getId());
+        return "redirect:/people";
+    }
+}
