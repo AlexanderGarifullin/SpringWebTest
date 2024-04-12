f67eff1cbee9102b0163e755db2c841881fe5a86
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-12 19:52:37 +0500

    feat: make PersonValidator

---
+++ src/main/java/hse/example/mvc/util/PersonValidator.java
@@ -0,0 +1,34 @@
+package hse.example.mvc.util;
+
+import hse.example.mvc.dao.PersonDAO;
+import hse.example.mvc.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class PersonValidator implements Validator {
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Person person = (Person) target;
+
+        if (personDAO.show(person.getEmail()).isPresent()) {
+            errors.rejectValue("email", "", "This email is already taken");
+        }
+
+    }
+}
